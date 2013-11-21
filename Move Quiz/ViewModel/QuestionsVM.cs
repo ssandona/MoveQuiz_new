f2aa18a6a0867dd851752d5fd264712265502a03
ssandona <stefanosandona1992@gmail.com>
2013-11-21 13:06:22 +0100

    Sistemato saltataggio livelli finiti

--- Move Quiz/ViewModel/QuestionsVM.cs
+++ Move Quiz/ViewModel/QuestionsVM.cs
@@ -1,28 +1,36 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO.IsolatedStorage;
 
 namespace Move_Quiz.ViewModel
 {
     public class QuestionsVM : INotifyPropertyChanged
     {
+        /// VAR: Isolate storage per salvare/caricare
+        private IsolatedStorageSettings appSettings = IsolatedStorageSettings.ApplicationSettings;
         private Livello actLiv;
         private List<Question> domande;
         private Question actQuestion;
         private int num_actQuestion;
 
-        public QuestionsVM (int liv){
+        public QuestionsVM(int liv)
+        {
             actLiv = App.getLivello(liv);
             domande = actLiv.Domande;
             actQuestion = domande[0];
             num_actQuestion = 1;
         }
 
-        public int Num_actQuestion {
-            get {
+        public int Num_actQuestion
+        {
+            get
+            {
                 return num_actQuestion;
             }
-            set {
-                if (value != num_actQuestion) {
+            set
+            {
+                if (value != num_actQuestion)
+                {
                     num_actQuestion = value;
                     RaisePropertyChanged("Num_actQuestion");
                 }
@@ -32,47 +40,62 @@ namespace Move_Quiz.ViewModel
 
         public bool nextQuestion(int punti)
         {
-            int i=domande.IndexOf(actQuestion);
+            int i = domande.IndexOf(actQuestion);
             if (i < (domande.Count - 1))
             {
                 ActQuestion = domande[i + 1];
-                int a=num_actQuestion+1;
+                int a = num_actQuestion + 1;
                 Num_actQuestion = a;
                 return true;
             }
-            else { 
-                actLiv.Best_Score=punti.ToString();
+            else
+            {
+                actLiv.Best_Score = punti.ToString();
                 Num_actQuestion = 1;
-                return false; 
+
+                /// Aggiungo alle settings bestscore+id
+                if (appSettings.Contains("bestscore" + actLiv.Id))
+                {
+                    /// Rimuovi vecchio best score
+                    appSettings.Remove("bestscore" + actLiv.Id);
+                }
+                /// Aggiungi nuovo best score
+                appSettings.Add("bestscore" + actLiv.Id, actLiv.Best_Score);
+                return false;
             }
         }
 
-        public void Ricomincia() {
+        public void Ricomincia()
+        {
             ActQuestion = domande[0];
             Num_actQuestion = 1;
         }
 
-        public bool Verify(int risp) {
+        public bool Verify(int risp)
+        {
             return actQuestion.isCorrect(risp);
         }
 
         public Question ActQuestion
         {
-            get {
+            get
+            {
                 return actQuestion;
             }
 
-            set {
-                if (value != actQuestion) {
+            set
+            {
+                if (value != actQuestion)
+                {
                     actQuestion = value;
                     RaisePropertyChanged("ActQuestion");
-                    for (int i = 1; i < 5; i++ )
+                    for (int i = 1; i < 5; i++)
                     {
                         RaisePropertyChanged("Risposta_" + i);
                     }
 
                 }
-                
+
             }
         }
 
